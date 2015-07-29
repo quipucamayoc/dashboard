197e28fcd503f85c052ab44f9c9f2dc1b1808320
Boris Kourtoukov <boriskourt@gmail.com>
2015-07-29 16:28:51 -0400

    Subtree

--- src/dashboard/display.cljs
+++ src/dashboard/display.cljs
@@ -4,172 +4,174 @@
   Uses the blessed and blessed-contrib libraries for node."
   (:require [cljs.nodejs :as nodejs]
             [dashboard.comm :as comm]
+            [dashboard.network]
             [cljs.core.async :as a :refer [<!]]
             [goog.object :as o])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
-(defonce dashboard (atom {}))
-
-(defonce blessed
-         (nodejs/require "blessed"))
-
-(defonce contrib
-         (nodejs/require "blessed-contrib"))
-
-(defonce screen
-         (.screen blessed))
-
-(defonce grid
-         (new contrib.grid #js {:rows 12 :cols 12 :screen screen}))
-
-(defn- create-layout []
-  (reset! dashboard
-          {:device-table
-           (.set grid 0 0 6 6
-                 contrib.table
-                 #js {:label         "Connected Beans"
-                      :fg            "green"
-                      :columnWidth   #js [10 5 5 5]
-                      :columnSpacing 1
-                      :keys          true})
-           :event-log
-           (.set grid 6 0 6 6
-                 contrib.log
-                 #js {:label      "Event Log"
-                      :fg         "green"
-                      :selectedFg "green"})
-           :sensor-history
-           (.set grid 0 0 12 6
-                 contrib.sparkline
-                 #js {:label "Sensor History"
-                      :tags  true
-                      :style #js {:fg "blue"}})}))
-
-(defonce device-table
-         (.set grid 0 0 6 6
-                            contrib.table
-                            #js {:label         "Connected Beans"
-                                 :fg            "green"
-                                 :columnWidth   #js [10 5 5 5]
-                                 :columnSpacing 1
-                                 :keys          true}))
-(defonce sensor-history
-  (.set grid 0 0 12 6
-        contrib.sparkline
-        #js {:label "Sensor History"
-             :tags  true
-             :style #js {:fg "blue"}}))
-
-(defonce event-log
-         (.set grid 6 0 6 6
-               contrib.log
-               #js {:label      "Event Log"
-                    :fg         "green"
-                    :selectedFg "green"}))
-
-(defn prog-log
-  "Prints data from the program into the dashboard."
-  [& args]
-  (let [entry (apply str args)]
-    (.log (:event-log @dashboard) entry)))
-
-(defn osc-log
-  "Prints what is sent via OSC into the dashboard."
-  [& args]
-  (let [entry (apply str args)]
-    #_(.log (:event-log @dashboard) entry)))
-
-(defn start-osc-log
-  "Listens for new messages on comm/osc-chan."
-  []
-  #_(go-loop []
-           (when-let [v (<! comm/osc-chan)]
-             (comment osc-log (:msg v))                             ;; :msg contains the above :response
-             (recur))))
-
-(defn start-prog-log
-  "Listens for new messages on comm/log-chan."
-  []
-  #_(go-loop []
-           (when-let [v (<! comm/log-chan)]
-             (prog-log (:msg v))                            ;; :msg contains the above :response
-             (recur))))
-
-(defn readable-device-key
-  "Takes the device UUID and displays just the first four characters."
-  [device-key]
-  (->> device-key
-       name
-       (take 4)
-       (apply str)))
-
-(defn clean-axis [axis]
-  (concat [350 0]
-          (if (nil? axis)
-            [0]
-            axis)))
-
-(defn device-history-data
-  "Takes a device map and returns axis data. Todo, return all sensors agnostically."
-  [[_ {:keys [sensors]}]]
-  (let [{x :x y :y z :z} sensors]
-    [(clean-axis x) (clean-axis y) (clean-axis z)]))
-
-(defn device-history-names
-  "Merges device UUIDs and sensor names."
-  [peripherals]
-  (apply concat
-         (map (fn [device-key]
-                (map #(str ":  " (readable-device-key device-key) " { " (name %) " }  ")
-                     (keys (:sensors (device-key peripherals)))))
-              (keys peripherals))))
-
-(defn clean-last-axis [axis]
-  (let [last-axis (last axis)]
-    (if (nil? last-axis)
-      0
-      last-axis)))
-
-(defn build-row
-  "Returns a row containing only latest sensor values from device. TO-DO: Remove hardcoded sensors"
-  [[_ {:keys [localName sensors]}]]
-  (let [{x :x y :y z :z} sensors]
-    [(str localName) (clean-last-axis x) (clean-last-axis y) (clean-last-axis z)]))
-
-(defn update-graphs
-  "Updates the gaphics that represent real-time & over-time device status and sensor values."
-  [peripherals]
-  (let [hist-header (device-history-names peripherals)
-        hist-data (mapv vec (apply concat (map device-history-data peripherals)))
-        device-table-data (mapv build-row peripherals)]
-
-    ; Update list of devices and sensor values.
-    (.setData (:device-table @dashboard)
-              (clj->js {:headers ["name" "x" "y" "z"]
-                        :data    device-table-data}))
-    ; Update sparkline of sensor values.
-    (.setData (:sensor-history @dashboard)
-              (clj->js hist-header)
-              (clj->js hist-data))
-    ; Needed to update the screen when graphics change.
-    (.render screen)))
-
-(defn- init-data
-  "Starts the Dashboard." []
-  (let [hist-header (clj->js [])
-        hist-data (clj->js
-                    [[]])
-        table-data (clj->js
-                     {:headers ["name" "x" "y" "z"]
-                      :data    [["no-devices" 0 0 0]]})]
-    (.setData device-table table-data)
-    (.focus device-table)
-    (.setData sensor-history hist-header hist-data)))
-
-(defn exit-handler [keys]
-  (.key screen (clj->js keys)
-        (fn [ch key]
-          (.exit js/process 0))))
+;
+;(defonce dashboard (atom {}))
+;
+;(defonce blessed
+;         (nodejs/require "blessed"))
+;
+;(defonce contrib
+;         (nodejs/require "blessed-contrib"))
+;
+;(defonce screen
+;         (.screen blessed))
+;
+;(defonce grid
+;         (new contrib.grid #js {:rows 12 :cols 12 :screen screen}))
+;
+;(defn- create-layout []
+;  (reset! dashboard
+;          {:device-table
+;           (.set grid 0 0 6 6
+;                 contrib.table
+;                 #js {:label         "Connected Beans"
+;                      :fg            "green"
+;                      :columnWidth   #js [10 5 5 5]
+;                      :columnSpacing 1
+;                      :keys          true})
+;           :event-log
+;           (.set grid 6 0 6 6
+;                 contrib.log
+;                 #js {:label      "Event Log"
+;                      :fg         "green"
+;                      :selectedFg "green"})
+;           :sensor-history
+;           (.set grid 0 0 12 6
+;                 contrib.sparkline
+;                 #js {:label "Sensor History"
+;                      :tags  true
+;                      :style #js {:fg "blue"}})}))
+;
+;(defonce device-table
+;         (.set grid 0 0 6 6
+;                            contrib.table
+;                            #js {:label         "Connected Beans"
+;                                 :fg            "green"
+;                                 :columnWidth   #js [10 5 5 5]
+;                                 :columnSpacing 1
+;                                 :keys          true}))
+;(defonce sensor-history
+;  (.set grid 0 0 12 6
+;        contrib.sparkline
+;        #js {:label "Sensor History"
+;             :tags  true
+;             :style #js {:fg "blue"}}))
+;
+;(defonce event-log
+;         (.set grid 6 0 6 6
+;               contrib.log
+;               #js {:label      "Event Log"
+;                    :fg         "green"
+;                    :selectedFg "green"}))
+;
+;(defn prog-log
+;  "Prints data from the program into the dashboard."
+;  [& args]
+;  (let [entry (apply str args)]
+;    (.log (:event-log @dashboard) entry)))
+;
+;(defn osc-log
+;  "Prints what is sent via OSC into the dashboard."
+;  [& args]
+;  (let [entry (apply str args)]
+;    #_(.log (:event-log @dashboard) entry)))
+;
+;(defn start-osc-log
+;  "Listens for new messages on comm/osc-chan."
+;  []
+;  #_(go-loop []
+;           (when-let [v (<! comm/osc-chan)]
+;             (comment osc-log (:msg v))                             ;; :msg contains the above :response
+;             (recur))))
+;
+;(defn start-prog-log
+;  "Listens for new messages on comm/log-chan."
+;  []
+;  #_(go-loop []
+;           (when-let [v (<! comm/log-chan)]
+;             (prog-log (:msg v))                            ;; :msg contains the above :response
+;             (recur))))
+;
+;(defn readable-device-key
+;  "Takes the device UUID and displays just the first four characters."
+;  [device-key]
+;  (->> device-key
+;       name
+;       (take 4)
+;       (apply str)))
+;
+;(defn clean-axis [axis]
+;  (concat [350 0]
+;          (if (nil? axis)
+;            [0]
+;            axis)))
+;
+;(defn device-history-data
+;  "Takes a device map and returns axis data. Todo, return all sensors agnostically."
+;  [[_ {:keys [sensors]}]]
+;  (let [{x :x y :y z :z} sensors]
+;    [(clean-axis x) (clean-axis y) (clean-axis z)]))
+;
+;(defn device-history-names
+;  "Merges device UUIDs and sensor names."
+;  [peripherals]
+;  (apply concat
+;         (map (fn [device-key]
+;                (map #(str ":  " (readable-device-key device-key) " { " (name %) " }  ")
+;                     (keys (:sensors (device-key peripherals)))))
+;              (keys peripherals))))
+;
+;(defn clean-last-axis [axis]
+;  (let [last-axis (last axis)]
+;    (if (nil? last-axis)
+;      0
+;      last-axis)))
+;
+;(defn build-row
+;  "Returns a row containing only latest sensor values from device. TO-DO: Remove hardcoded sensors"
+;  [[_ {:keys [localName sensors]}]]
+;  (let [{x :x y :y z :z} sensors]
+;    [(str localName) (clean-last-axis x) (clean-last-axis y) (clean-last-axis z)]))
+;
+;(defn update-graphs
+;  "Updates the gaphics that represent real-time & over-time device status and sensor values."
+;  [peripherals]
+;  (let [hist-header (device-history-names peripherals)
+;        hist-data (mapv vec (apply concat (map device-history-data peripherals)))
+;        device-table-data (mapv build-row peripherals)]
+;
+;    ; Update list of devices and sensor values.
+;    (.setData (:device-table @dashboard)
+;              (clj->js {:headers ["name" "x" "y" "z"]
+;                        :data    device-table-data}))
+;    ; Update sparkline of sensor values.
+;    (.setData (:sensor-history @dashboard)
+;              (clj->js hist-header)
+;              (clj->js hist-data))
+;    ; Needed to update the screen when graphics change.
+;    (.render screen)))
+;
+;(defn- init-data
+;  "Starts the Dashboard." []
+;  (let [hist-header (clj->js [])
+;        hist-data (clj->js
+;                    [[]])
+;        table-data (clj->js
+;                     {:headers ["name" "x" "y" "z"]
+;                      :data    [["no-devices" 0 0 0]]})]
+;    (.setData device-table table-data)
+;    (.focus device-table)
+;    (.setData sensor-history hist-header hist-data)))
+;
+;(defn exit-handler [keys]
+;  (.key screen (clj->js keys)
+;        (fn [ch key]
+;          (.exit js/process 0))))
 
 (defn main []
   ;(create-layout)
@@ -178,7 +180,7 @@
   ;(init-data)
   ;(exit-handler ["escape" "q" "C-c"])
   ;(.render screen)
-  (comm/init)
+  (dashboard.network/init)
   )
 
 (set! *main-cli-fn* main)
--- src/dashboard/network.cljs
+++ src/dashboard/network.cljs
@@ -1,9 +1,16 @@
 (ns dashboard.network
   (:require [cljs.nodejs :as nodejs]
+            [cljs.reader :as edn]
             [cljs.core.async :as a :refer [<!]])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
+(enable-console-print!)
 
+(defonce fs (nodejs/require "fs"))
+
+(defonce port-config (.readFile fs "port-record/mpr.edn" "utf8"
+                                (fn [err data]
+                                  (println (edn/read-string data)))))
 
 (defn init []
-  )
+  #_(.log js/console port-config))
